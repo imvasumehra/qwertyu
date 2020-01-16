75f0fa71fc9603ae7bbe31d09e3fe9c3bde95beb
Vasu Mehra <vasumail007@gmail.com>
2020-01-16 19:33:01 +0530

    Asdf

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+var port_number = process.env.PORT || 3000;
+
+
 app.get('/', function (req, res) {
   res.render('index');
   setInterval(function() {
@@ -61,7 +64,7 @@ app.get('/addtask', function (req, res) {
   res.render('index');
 });
 
-app.listen(3000, function () {
+app.listen(port_number, function () {
   console.log('Example app listening on port 3000!')
 });
 
